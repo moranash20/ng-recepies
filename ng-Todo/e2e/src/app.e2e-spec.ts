1b5a5ddf8e9d0f3d61b9a9fea4958da9637278cd
מורן אשכנזי - טולידנו <moranash20@gmail.com>
2021-03-17 14:35:20 +0200

    create new project ngTodo

--- ng-Todo/e2e/src/app.e2e-spec.ts
+++ ng-Todo/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('ng-todo app is running!');
+    expect(await page.getTitleText()).toEqual('ng-Todo app is running!');
   });
 
   afterEach(async () => {
