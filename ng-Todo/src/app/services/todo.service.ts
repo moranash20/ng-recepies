de33435fb0e7fd0f946e770fce4fad318aef790e
מורן אשכנזי - טולידנו <moranash20@gmail.com>
2021-03-21 09:58:45 +0200

    module2 Lesson19+20 added several components

--- ng-Todo/src/app/services/todo.service.ts
+++ ng-Todo/src/app/services/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { ITodo } from '../models/todo.interface';
 
 @Injectable({
@@ -9,43 +9,62 @@ export class TodoService {
 
   private mock: ITodo[] = [
     {
+      id: 1,
       title:"Grizzly bear",
       description:"Ursus arctos",
-      isCompleted:true,
+      isCompleted:false,
       isArchived:true,
-      endDate: 3/13/2021
+      endDate: 3/13/2021,
+      selected: true
     },
     {
+      id: 2,
       title:"Grenadier, purple","description":"Uraeginthus granatina",
-      isCompleted:true,
+      isCompleted:false,
       isArchived:true,
-      endDate: 10/16/2020
+      endDate: 10/16/2020,
+      selected: false
     },
     {
+      id: 3,
       title:"Lion, southern sea","description":"Otaria flavescens",
       isCompleted:false,
       isArchived:true,
-      endDate: 11/2/2020
+      endDate: 11/2/2020,
+      selected: false
     },
     {
+      id: 4,
       title:"Vulture, griffon","description":"Gyps fulvus",
-      isCompleted:true,
+      isCompleted:false,
       isArchived:false,
-      endDate: 4/14/2020
+      endDate: 4/14/2020,
+      selected: false
     },
     {
+      id: 5,
       title:"Ass, asiatic wild","description":"Equus hemionus",
       isCompleted:false,
       isArchived:false,
-      endDate: 9/10/2020
+      endDate: 9/10/2020,
+      selected: false
     },
   ];
 
   private _todoSubject: BehaviorSubject<Array<ITodo>> = new BehaviorSubject(this.mock);
 
+  private _singleTodoSubject: BehaviorSubject<ITodo> = new BehaviorSubject(this.mock[0])
   constructor() { }
 
   public getTodos(): Observable<Array<ITodo>>{
     return this._todoSubject.asObservable();
+  }
+
+  public getSelectedTodo(): Observable<ITodo>{
+    return this._singleTodoSubject.asObservable();
+  }
+
+  public setSelectedTodo(todo: ITodo){
+    this._singleTodoSubject.next(todo);
   }
 }
