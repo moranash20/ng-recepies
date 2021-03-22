ad5449f2eb1ecf109003a4de76dddc812d47e694
מורן אשכנזי - טולידנו <moranash20@gmail.com>
2021-03-22 09:00:25 +0200

    fix Bug

--- ng-Todo/src/app/components/todo/todo.component.ts
+++ ng-Todo/src/app/components/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit , } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ITodo } from 'src/app/models/todo.interface';
 import { TodoService } from 'src/app/services/todo.service';
@@ -6,35 +6,32 @@ import { TodoService } from 'src/app/services/todo.service';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
-
-  @Input() set todo(todo: ITodo){
+  @Input() set todo(todo: ITodo) {
     this._todo = todo;
-    console.log(todo);
   }
 
-  get todo(){
+  get todo() {
     return this._todo;
   }
 
   private _todo: ITodo;
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-  
+    console.log(this.todo.endDate);
   }
 
-
-  public onCompleteTodo (todo: ITodo): void{
-    //todo.isCompleted = true;
-    this.todoService.onTodoAction(todo.id, "isCompleted");
+  public onCompleteTodo(todo: ITodo): void {
+    // todo.isCompleted = true;
+    this.todoService.onTodoAction(todo.id, 'isCompleted');
   }
 
-  public onArchiveTodo(): void{
+  public onArchiveTodo(): void {
     //this.todo.isArchived = true;
-    this.todoService.onTodoAction(this.todo.id, "isArchived");
+    this.todoService.onTodoAction(this.todo.id, 'isArchived');
   }
 }
